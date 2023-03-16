21eb48465064ea386c211374fe45b0e379d1a35d
kreslavskiy <mishak1000236@gmail.com>
2023-03-16 20:29:41 +0200

    Tests improved

--- test/linkedList.test.ts
+++ test/linkedList.test.ts
@@ -1,4 +1,4 @@
-import LinkedList from '../src/linkedListBasedOnArray';
+import LinkedList from '../src/linkedList';
 
 describe('Linked list based on array', () => {
   let linkedList;
@@ -47,23 +47,23 @@ describe('Linked list based on array', () => {
   });
 
   it('should delete node by index', () => {
-    expect(linkedList.getLength()).toBe(3);
+    expect(linkedList.getLength()).toBe(5);
 
     const deleted = linkedList.delete(1);
 
     expect(deleted.value).toBe('Node between 1st and 2nd');
-    expect(linkedList.getLength()).toBe(2);
+    expect(linkedList.getLength()).toBe(4);
   });
 
   it('should delete all nodes with some value', () => {
     linkedList.append('First node');
     linkedList.append('First node');
 
-    expect(linkedList.getLength()).toBe(4);
+    expect(linkedList.getLength()).toBe(6);
 
     linkedList.deleteAll('First node');
 
-    expect(linkedList.getLength()).toBe(1);
+    expect(linkedList.findFirstByValue('First node')).toBe(-1);
   });
 
   it('should clone the list', () => {
@@ -75,24 +75,24 @@ describe('Linked list based on array', () => {
     linkedList.append('First node');
 
     expect(linkedList.getByIndex(0).value).toBe('Second node');
-    expect(linkedList.getByIndex(1).value).toBe('First node');
+    expect(linkedList.getByIndex(3).value).toBe('First node');
 
     linkedList.reverse();
 
     expect(linkedList.getByIndex(0).value).toBe('First node');
-    expect(linkedList.getByIndex(1).value).toBe('Second node');
+    expect(linkedList.getByIndex(3).value).toBe('Second node');
   });
 
   it('should extend the list', () => {
     const secondList = linkedList.clone();
-    expect(linkedList.getLength()).toBe(2);
-    expect(secondList.getLength()).toBe(2);
+    expect(linkedList.getLength()).toBe(4);
+    expect(secondList.getLength()).toBe(4);
 
     linkedList.extend(secondList);
-    expect(linkedList.getLength()).toBe(4);
+    expect(linkedList.getLength()).toBe(8);
 
     secondList.append('Third node in 2nd list. Should not be in 1st list');
-    expect(secondList.getLength()).toBe(3);
-    expect(linkedList.getLength()).not.toBe(5);
+    expect(secondList.getLength()).toBe(5);
+    expect(linkedList.getLength()).not.toBe(9);
   });
 });
